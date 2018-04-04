6da12b548b58f7ea7797e303569226c8000a7ff6
lik1236 <kevinli@Kevins-MacBook-Pro.local>
2018-04-03 22:22:53 -0700

    unit 15
    owejg;oweijgoij

---
+++ unit15Git/src/Paddle.java
@@ -0,0 +1,80 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle extends Block
+{
+   //instance variables
+   private int speed;
+
+   public Paddle()
+   {
+		super(10,10);
+      speed =5;
+   }
+   public Paddle(int x,int y){
+	   super(x,y);
+	   speed = 5;
+   }
+   
+   public Paddle(int x, int y, int s){
+	   super(x,y);
+	   speed = s;
+   }
+   
+   public Paddle(int x, int y, int w, int h, int s){
+	   super(x,y,w,h);
+	   speed = s;
+   }
+   
+   public Paddle(int x, int y, int w, int h, Color col, int s){
+	   super(x,y,w,h, col);
+	   speed = s;
+   }
+
+
+   //add the other Paddle constructors
+
+
+public void setSpeed(int s) {
+	speed = s;
+}
+
+
+
+
+
+
+
+   public void moveUpAndDraw(Graphics window)
+   {
+	   draw(window,Color.WHITE);
+	   setY(getY()-speed);
+	   draw(window, Color.BLACK);
+
+
+   }
+
+   public void moveDownAndDraw(Graphics window)
+   {
+	   draw(window, Color.WHITE);
+	   setY(getY()+speed);
+	   draw(window, Color.BLACK);
+
+
+   }
+
+   public int getSpeed() {
+	   return speed;
+   }
+   
+   
+   public String toString() {
+	   return super.toString() + speed;
+   }
+}
